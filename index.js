d842f2b542513cbd065d6a59811d4672c0d80ce9
MARVIN DE LA PEÑA <marvin.dlpg@gmail.com>
2023-12-21 22:31:38 -0500

    Update Index.js [main] [22:30]

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
 app.get('/', (req,res) =>{ 
-    res.send('Sigp - Mis Hermosos Joseph Marc y Ariana Michelle [Hora: 22:09]'); 
+    res.send('Sigp - Mis Hermosos Joseph Marc y Ariana Michelle [Hora: 22:30]'); 
 });
 
 app.use('/api', apiRouter);
