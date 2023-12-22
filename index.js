e71293234ca92aa8e2f9c231a82bf7a49da27c5c
MARVIN DE LA PEÑA <marvin.dlpg@gmail.com>
2023-12-21 21:23:46 -0500

    Update index.js [main] [21:24]

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
 app.get('/', (req,res) =>{ 
-    res.send('Sigp - Sistema de Gestión de Procesos'); 
+    res.send('Sigp - Sistema de Gestión de Procesos My bebe Hermoso Joseph'); 
 });
 
 app.use('/api', apiRouter);
