612a28b006607e9457211b359542eda26cd88db3
MARVIN DE LA PEÑA <marvin.dlpg@gmail.com>
2024-01-24 12:04:46 -0500

    Update index.html  Enero 24-2024

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
 app.get('/', (req,res) =>{ 
-    res.send('Sigp - Te Amo mi amor y a nuestros hermosos Ariana y Joseph Marc Enero 2024 [Hora: 17:27]'); 
+    res.send('Sigp - Educational platform 2024'); 
 });
 
 app.use('/api', apiRouter);
 
 app.listen(3000, () => { 
-    console.log('Server On Line 02 de Enero 17:27'); 
+    console.log('Server On Line 02 de Enero 24 - 2024'); 
 });
